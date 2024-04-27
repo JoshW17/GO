65f5e09816fca9a845e64eab86d6b5151dffd65b
joshie <89046683+JoshW17@users.noreply.github.com>
2024-04-26 22:03:56 -0400

    very simple webserver

---
+++ webserver/webserver.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported.", http.StatusNotFound)
+		return
+	}
+
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+	}
+
+	fmt.Println("Hello There! - Obi Wan")
+
+}
+
+func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Println("Starting Webserver on port 8080 ...")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
