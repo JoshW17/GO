c837e4e9f0d6c905751061a2e50a524ec5ba88ef
joshie <89046683+JoshW17@users.noreply.github.com>
2024-04-28 02:42:08 -0400

    make a working login page

--- webserver/webserver.go
+++ webserver/webserver.go
@@ -22,11 +22,40 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Method not supported.", http.StatusMethodNotAllowed)
+		return
+	}
+
+	if r.URL.Path != "/login" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+	}
+
+	err := r.ParseForm()
+	if err != nil {
+		fmt.Fprintf(w, "ParseForm() error: %v", err)
+		return
+	}
+
+	fmt.Println("POST request successful!")
+
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+
+	if (username == "admin") && (password == "admin") {
+		http.ServeFile(w, r, "./static/success.html")
+	} else {
+		http.ServeFile(w, r, "./static/fail.html")
+	}
+}
+
 func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
 
 	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/login", loginHandler)
 
 	fmt.Println("Starting Webserver on port 8080 ...")
 	err := http.ListenAndServe(":8080", nil)
