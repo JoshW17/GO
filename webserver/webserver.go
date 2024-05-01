4de38389a51fb0aeb3d678d67cf5f8d3a14fff8b
joshie <89046683+JoshW17@users.noreply.github.com>
2024-04-30 22:37:29 -0400

    templates...finally templates
    this took me 3 days btw

--- webserver/webserver.go
+++ webserver/webserver.go
@@ -2,13 +2,20 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
+	"time"
 )
+
+type dash struct {
+	Title string
+	Body  string
+}
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "GET" {
-		http.Error(w, "Method is not supported.", http.StatusNotFound)
+		http.Error(w, "Method is not supported.", http.StatusMethodNotAllowed)
 		return
 	}
 
@@ -44,9 +51,43 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 	password := r.FormValue("password")
 
 	if (username == "admin") && (password == "admin") {
+		expiration := time.Now().Add(365 * 24 * time.Hour)
+		cookie := http.Cookie{Name: "accessToken", Value: username, Expires: expiration, Path: "/", MaxAge: 90000, Secure: true, HttpOnly: true}
+		http.SetCookie(w, &cookie)
 		http.ServeFile(w, r, "./static/success.html")
+		fmt.Println("Cookie has been set.")
 	} else {
 		http.ServeFile(w, r, "./static/fail.html")
+	}
+}
+
+func checkIfGET(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		http.Error(w, "Method not supported.", http.StatusMethodNotAllowed)
+	}
+}
+
+func dashboardHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, "Method not supported.", http.StatusMethodNotAllowed)
+	}
+
+	cookie, err := r.Cookie("accessToken")
+
+	if err != nil {
+		fmt.Fprintf(w, "cookie error: %v", err)
+		return
+	}
+
+	if cookie.Value == "admin" {
+		userDash := dash{Title: cookie.Value + "'s Dashoard", Body: "This is some sample body text"}
+		t := template.Must(template.New("dashboards").ParseFiles("static/dashboard.html"))
+		err = t.ExecuteTemplate(w, "dashboard.html", userDash)
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		http.Error(w, "You do not have access to this page.", http.StatusForbidden)
 	}
 }
 
@@ -56,6 +97,10 @@ func main() {
 
 	http.HandleFunc("/hello", helloHandler)
 	http.HandleFunc("/login", loginHandler)
+	http.HandleFunc("/success.html", checkIfGET)
+	http.HandleFunc("/dashboard", dashboardHandler)
+	rh := http.RedirectHandler("/dashboard", http.StatusMisdirectedRequest)
+	http.Handle("/dashboard.html", rh)
 
 	fmt.Println("Starting Webserver on port 8080 ...")
 	err := http.ListenAndServe(":8080", nil)
